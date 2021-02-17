9815c12367be38021e12101d92450bd97bb68ed3
ELO NARA <elonara@MacBook-Air.local>
2021-02-17 17:30:12 +0100

    gatsby-node.js updated window error

--- gatsby-node.js
+++ gatsby-node.js
@@ -81,3 +81,19 @@ module.exports.onCreateNode = async ({ node, actions, createNodeId }) => {
     });
   }
 };
+
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
